0c2b7f249270d9c80208f600a227e7760a8b72bb
StrathCole <strathcole@gmail.com>
2025-11-23 17:36:34 +0100

    chore: bump version to 1.0.5

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@
 package version
 
 // Version is the current version of the oracle-go application.
-const Version = "1.0.4"
+const Version = "1.0.5"
 
 // AgentString returns the full agent string with versioning.
 // Format: @classic-terra/oracle-go@v{version}.
